472d2e533162ae781fdb2ec85d5d97ad0c339b95
Krasilnikov Roman <x0kill@ya.ru>
2024-05-21 23:33:35 +0300

    Example

--- src/example/code.php
+++ src/example/code.php
@@ -1,15 +1,31 @@
 <?php
 
-final class CatBankProcessor implements PaymentProcessorInterface
-{
+enum PaymentSystemType: string {
+    case PAYPAL = 'paypal';
+    case WEBMONEY = 'webmoney';
+    case CAT_BANK = 'cat-bank';
+}
 
-    public function payout()
-    {
-        echo 'Payout from CatBank';
-    }
 
-    public function payment()
-    {
-        echo 'Payment from CatBank';
-    }
+
+
+
+// Выполните операцию payment для платежной системы `type`
+function case1(PaymentSystemType $type, int $amount): int {
+
+}
+
+enum PaymentSystemActionType: string {
+    case PAYMENT = 'payment';
+    case PAYOUT = 'payout';
+}
+
+
+
+
+
+
+// Выполните операцию `action` для платежной системы `type`
+function case2(PaymentSystemType $type, PaymentSystemActionType $action, int $amount): int {
+
 }
