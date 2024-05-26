d4b340acaba5a26f0d39c1bf715485f72c0c7b4c
Krasilnikov Roman <x0kill@ya.ru>
2024-05-27 00:10:35 +0300

    WIP: Cases buttons

--- src/components/editor.svelte
+++ src/components/editor.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { onMount } from "svelte";
   import { editor } from "monaco-editor";
 
   import code from "@/example/code.php?raw";
@@ -8,7 +7,7 @@
 
   let codeEditorElement: HTMLDivElement;
 
-  onMount(() => {
+  $effect(() => {
     editor.create(codeEditorElement, {
       model: model,
       theme: "vs-dark",
@@ -17,16 +16,48 @@
     });
   });
 
-  function log() {
-    alert("Hello!");
+  enum CaseStatus {
+    Idle = "idle",
+    Running = "running",
+    Success = "success",
+    Error = "error",
   }
+
+  const cases = $state([
+    {
+      id: "1",
+      name: "Case 1",
+      status: CaseStatus.Idle,
+    },
+    {
+      id: "2",
+      name: "Case 2",
+      status: CaseStatus.Idle,
+    },
+  ]);
 </script>
 
 <div bind:this={codeEditorElement} class="grow"></div>
 <div class="p-4 border-t border-base-100 flex items-center gap-3">
-  <button onclick={log} class="btn btn-sm btn-primary"> Run </button>
-  <button class="btn btn-sm btn-secondary"> Case 1 </button>
-  <button class="btn btn-sm btn-secondary"> Case 2 </button>
+  <button class="btn btn-sm btn-primary"> Run </button>
+  {#each cases as testCase (testCase.id)}
+    <button
+      class="btn btn-sm btn-secondary"
+      class:btn-success={testCase.status === CaseStatus.Success}
+      class:btn-error={testCase.status === CaseStatus.Error}
+      onclick={async () => {
+        if (testCase.status === CaseStatus.Running) {
+          return;
+        }
+        testCase.status = CaseStatus.Running;
+      }}
+    >
+      {testCase.name}
+      {#if testCase.status === CaseStatus.Running}
+        <span class="loading loading-spinner"></span>
+      {/if}
+    </button>
+  {/each}
   <select class="select select-ghost select-sm ml-auto">
     <option> PHP 8.2.11 </option>
   </select>
