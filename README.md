3a6c1c32e5d513f4f04140627c2311de6ef2a3de
Krasilnikov Roman <x0kill@ya.ru>
2024-06-28 22:56:04 +0300

    Dev env

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Programming Patterns Practice
+
+## Development
+
+The artifacts saved in the repository must be created in the nix development environment.
+
+```console
+nix develop
+mk a/build
+```
+
+## See also
+
+- Simple build automation tool [mk](https://github.com/x0k/mk)
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    mk.url = "github:x0k/mk";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      mk,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        inherit nixpkgs;
+        buildInputs = [
+          mk.packages.${system}.default
+          pkgs.nodejs
+          pkgs.pnpm
+          pkgs.go
+        ];
+      };
+    };
+}
