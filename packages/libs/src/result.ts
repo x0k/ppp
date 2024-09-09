8df354017e444b40bc6a349eb7cbbe19abf8fc5a
Krasilnikov Roman <x0kill@ya.ru>
2024-09-09 18:34:18 +0300

    Unwrap function

--- packages/libs/src/result.ts
+++ packages/libs/src/result.ts
@@ -1,27 +1,35 @@
 export interface Ok<T> {
-  ok: true
-  value: T
+  ok: true;
+  value: T;
 }
 
 export interface Err<E> {
-  ok: false
-  error: E
+  ok: false;
+  error: E;
 }
 
-export type Result<T, E> = Ok<T> | Err<E>
+export type Result<T, E> = Ok<T> | Err<E>;
 
 export function isOk<T, E>(result: Result<T, E>): result is Ok<T> {
-  return result.ok
+  return result.ok;
 }
 
 export function isErr<T, E>(result: Result<T, E>): result is Err<E> {
-  return !result.ok
+  return !result.ok;
 }
 
 export function ok<T>(value: T): Ok<T> {
-  return { ok: true, value }
+  return { ok: true, value };
 }
 
 export function err<E>(error: E): Err<E> {
-  return { ok: false, error }
+  return { ok: false, error };
+}
+
+export function unwrap<T, E>(g: Generator<Result<T, E>, Result<T, E>, T>) {
+  let result = g.next();
+  while (!result.done && isOk(result.value)) {
+    result = g.next(result.value.value);
+  }
+  return result.value;
 }
