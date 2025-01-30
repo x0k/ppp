060a3ff0c0fe0476aa87d7dff16b02b80521973c
Roman Krasilnikov <x0kill@ya.ru>
2025-01-30 22:07:00 +0300

    Implement stdin for java

--- packages/java-runtime/src/jvm-factory.ts
+++ packages/java-runtime/src/jvm-factory.ts
@@ -1,8 +1,12 @@
+import BrowserFS from "browserfs";
 import { type Context, inContext } from "libs/context";
 import type { Streams } from "libs/io";
+import { patch } from "libs/patcher";
 
 import { createJVM, JVM } from "./jvm";
 import { process } from "./process.js";
+
+const { Buffer } = BrowserFS.BFSRequire("buffer");
 
 export type JVMFactory = (ctx: Context) => Promise<JVM>;
 
@@ -18,6 +22,9 @@ export function makeJVMFactory(streams: Streams): JVMFactory {
     const onStdout = (data: Uint8Array) => streams.out.write(data);
     const onStderr = (data: Uint8Array) => streams.err.write(data);
     // process.initializeTTYs();
+    const disposableRead = patch(process.stdin, "read", () => {
+      return Buffer.from(streams.in.read());
+    });
     process.stdout.on("data", onStdout);
     process.stderr.on("data", onStderr);
     const disposable = ctx.onCancel(() => {
@@ -25,6 +32,7 @@ export function makeJVMFactory(streams: Streams): JVMFactory {
       jvm.halt(1);
       process.stdout.removeListener("data", onStdout);
       process.stderr.removeListener("data", onStderr);
+      disposableRead[Symbol.dispose]();
     });
     return jvm;
   };
