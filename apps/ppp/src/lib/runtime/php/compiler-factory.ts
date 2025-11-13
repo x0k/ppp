9a89ac50da5837846199277bfe3f7160ef577ba6
Roman Krasilnikov <x0kill@ya.ru>
2025-11-13 18:34:13 +0300

    Update php runtime

--- apps/ppp/src/lib/runtime/php/compiler-factory.ts
+++ apps/ppp/src/lib/runtime/php/compiler-factory.ts
@@ -1,14 +1,22 @@
-import type { CompilerFactory, Program } from "libs/compiler";
-import { phpCompilerFactory, PHPProgram } from "php-runtime";
+import type { CompilerFactory, Program } from 'libs/compiler';
+import { phpCompilerFactory, PHPProgram } from 'php-runtime';
+
+import phpWasmUrl from 'php-runtime/php.wasm?url';
 
 export const makePhpCompiler: CompilerFactory<Program> = async (ctx, streams) => {
-  const php = await phpCompilerFactory(ctx);
-  return {
-    async compile(_, files) {
-      if (files.length !== 1) {
-        throw new Error("Compilation of multiple files is not implemented");
-      }
-      return new PHPProgram(files[0].content, php, streams);
-    },
-  };
+	const php = await phpCompilerFactory(ctx, async (info, resolve) => {
+		const { instance, module } = await WebAssembly.instantiateStreaming(
+			fetch(phpWasmUrl, { signal: ctx.signal, cache: 'force-cache' }),
+			info
+		);
+		resolve(instance, module);
+	});
+	return {
+		async compile(_, files) {
+			if (files.length !== 1) {
+				throw new Error('Compilation of multiple files is not implemented');
+			}
+			return new PHPProgram(files[0].content, php, streams);
+		}
+	};
 };
