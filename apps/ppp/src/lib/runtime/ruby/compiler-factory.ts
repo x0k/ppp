98121f34d580367e284cbddfab736163e0a7929b
Roman Krasilnikov <x0kill@ya.ru>
2025-11-20 01:20:48 +0300

    [app] Cache ruby wasm loader

--- apps/ppp/src/lib/runtime/ruby/compiler-factory.ts
+++ apps/ppp/src/lib/runtime/ruby/compiler-factory.ts
@@ -1,13 +1,18 @@
 import type { CompilerFactory, Program } from 'libs/compiler';
+import { createCachedFetch } from 'libs/fetch';
 import type { Streams } from 'libs/io';
+import { createLogger } from 'libs/logger';
 import { RubyProgram, createRubyVM } from 'ruby-runtime';
 
 import rubyWasmUrl from 'ruby-runtime/ruby.wasm?url';
 
 export const makeRubyCompiler: CompilerFactory<Streams, Program> = async (ctx, streams) => {
+	const logger = createLogger(streams.out);
+	const fetcher = createCachedFetch(await caches.open('ruby-cache'));
 	const rubyWasmModule = await WebAssembly.compileStreaming(
-		fetch(rubyWasmUrl, { signal: ctx.signal, cache: 'force-cache' })
+		fetcher(rubyWasmUrl, { signal: ctx.signal })
 	);
+	logger.info(`Loaded ${rubyWasmUrl}`);
 	return {
 		async compile(_, files) {
 			if (files.length !== 1) {
