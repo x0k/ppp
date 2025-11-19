f8e0ca562af530a3e47439b59a22e37fcf1f085c
Roman Krasilnikov <x0kill@ya.ru>
2025-11-20 00:50:22 +0300

    [app] Cache go wasm loader

--- apps/ppp/src/lib/runtime/go/test-compiler-factory.ts
+++ apps/ppp/src/lib/runtime/go/test-compiler-factory.ts
@@ -1,45 +1,53 @@
-import { inContext, type Context } from "libs/context";
-import type { Streams } from "libs/io";
-import type { TestCompiler } from "libs/testing";
+import type { Context } from 'libs/context';
+import type { Streams } from 'libs/io';
+import type { TestCompiler } from 'libs/testing';
+import { createLogger, type Logger } from 'libs/logger';
+import { createCachedFetch } from 'libs/fetch';
 import {
-  makeCompilerFactory,
-  makeGoCompilerFactory,
-  makeGoEvaluatorFactory,
-  GoTestProgram,
-} from "go-runtime";
+	makeCompilerFactory,
+	makeGoCompilerFactory,
+	makeGoEvaluatorFactory,
+	GoTestProgram
+} from 'go-runtime';
 
-import wasmInit from "go-runtime/compiler.wasm?init";
+import wasmUrl from 'go-runtime/compiler.wasm?url';
 
 export type GenerateCaseExecutionCode<I> = (input: I) => string;
 
 export class GoTestCompilerFactory {
-  constructor(protected readonly streams: Streams) {}
+	protected readonly logger: Logger;
 
-  async create<I, O>(
-    ctx: Context,
-    generateCaseExecutionCode: GenerateCaseExecutionCode<I>
-  ): Promise<TestCompiler<I, O>> {
-    class TestProgram extends GoTestProgram<I, O> {
-      protected override generateCaseExecutionCode(input: I): string {
-        return generateCaseExecutionCode(input);
-      }
-    }
-    const goEvaluatorFactory = makeGoEvaluatorFactory<O>(
-      makeGoCompilerFactory(
-        await makeCompilerFactory((imports) =>
-          inContext(ctx, wasmInit(imports))
-        )
-      )
-    );
-    return {
-      compile: async (ctx, files) => {
-        if (files.length !== 1) {
-          throw new Error("Compilation of multiple files is not implemented");
-        }
-        return new TestProgram(
-          await goEvaluatorFactory(ctx, this.streams, files[0].content)
-        );
-      },
-    };
-  }
+	constructor(protected readonly streams: Streams) {
+		this.logger = createLogger(streams.out);
+	}
+
+	async create<I, O>(
+		ctx: Context,
+		generateCaseExecutionCode: GenerateCaseExecutionCode<I>
+	): Promise<TestCompiler<I, O>> {
+		class TestProgram extends GoTestProgram<I, O> {
+			protected override generateCaseExecutionCode(input: I): string {
+				return generateCaseExecutionCode(input);
+			}
+		}
+		const fetcher = createCachedFetch(await caches.open('go-cache'));
+		const goEvaluatorFactory = makeGoEvaluatorFactory<O>(
+			makeGoCompilerFactory(
+				await makeCompilerFactory((imports) =>
+					WebAssembly.instantiateStreaming(fetcher(wasmUrl, { signal: ctx.signal }), imports).then(
+						(m) => m.instance
+					)
+				)
+			)
+		);
+	  this.logger.info(`Loaded ${wasmUrl}`);
+		return {
+			compile: async (ctx, files) => {
+				if (files.length !== 1) {
+					throw new Error('Compilation of multiple files is not implemented');
+				}
+				return new TestProgram(await goEvaluatorFactory(ctx, this.streams, files[0].content));
+			}
+		};
+	}
 }
