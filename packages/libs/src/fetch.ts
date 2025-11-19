b1450046dcd38c53f95499b8e1408c8ac3b1c222
Roman Krasilnikov <x0kill@ya.ru>
2025-11-20 00:33:36 +0300

    [libs] Accept request options in fetcher

--- packages/libs/src/fetch.ts
+++ packages/libs/src/fetch.ts
@@ -1,8 +1,8 @@
 export type Fetcher = (url: string) => Promise<Response>;
 
 export function createCachedFetch(cache: Cache) {
-  return async (url: string) => {
-    const request = new Request(url);
+  return async (url: string, init?: RequestInit) => {
+    const request = new Request(url, init);
     const cached = await cache.match(request);
     if (cached) {
       return cached.clone();
