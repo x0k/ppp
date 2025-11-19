830024594a94830c57c75352fab369cd2ec08f26
Roman Krasilnikov <x0kill@ya.ru>
2025-11-20 01:17:23 +0300

    [app] Cache python wasm loader

--- apps/ppp/src/lib/runtime/python/compiler-factory.ts
+++ apps/ppp/src/lib/runtime/python/compiler-factory.ts
@@ -1,18 +1,23 @@
 import type { CompilerFactory, Program } from 'libs/compiler';
+import { createCachedFetch } from 'libs/fetch';
 import type { Streams } from 'libs/io';
+import { createLogger } from 'libs/logger';
 import { PyProgram, pyRuntimeFactory } from 'python-runtime';
 
 import wasmUrl from 'python-runtime/pyodide.wasm?url';
 import stdlibUrl from 'python-runtime/python-stdlib.zip?url';
 
 export const makePythonCompiler: CompilerFactory<Streams, Program> = async (ctx, streams) => {
+	const logger = createLogger(streams.out);
+	const fetcher = createCachedFetch(await caches.open('python-cache'));
 	const pyRuntime = await pyRuntimeFactory(
 		ctx,
 		streams,
 		(ctx, imports) =>
-			WebAssembly.instantiateStreaming(fetch(wasmUrl, { signal: ctx.signal }), imports),
+			WebAssembly.instantiateStreaming(fetcher(wasmUrl, { signal: ctx.signal }), imports),
 		stdlibUrl
 	);
+	logger.info(`Loaded ${wasmUrl}`);
 	return {
 		async compile(_, files) {
 			if (files.length !== 1) {
--- apps/ppp/src/lib/runtime/python/test-compiler-factory.ts
+++ apps/ppp/src/lib/runtime/python/test-compiler-factory.ts
@@ -1,44 +1,47 @@
-import type { Context } from "libs/context";
-import type { Streams } from "libs/io";
-import type { TestCompiler } from "libs/testing";
-import { pyRuntimeFactory, PyTestProgram } from "python-runtime";
+import type { Context } from 'libs/context';
+import { createCachedFetch } from 'libs/fetch';
+import type { Streams } from 'libs/io';
+import { createLogger, type Logger } from 'libs/logger';
+import type { TestCompiler } from 'libs/testing';
+import { pyRuntimeFactory, PyTestProgram } from 'python-runtime';
 
-// @ts-ignore
-import wasmUrl from "python-runtime/pyodide.wasm";
-// @ts-ignore
-import stdlibUrl from "python-runtime/python-stdlib.zip";
+import wasmUrl from 'python-runtime/pyodide.wasm?url';
+import stdlibUrl from 'python-runtime/python-stdlib.zip?url';
 
 export type GenerateCaseExecutionCode<I> = (input: I) => string;
 
 export class PythonTestCompilerFactory {
-  constructor(protected readonly streams: Streams) {}
+	protected readonly logger: Logger;
 
-  async create<I, O>(
-    ctx: Context,
-    generateCaseExecutionCode: GenerateCaseExecutionCode<I>
-  ): Promise<TestCompiler<I, O>> {
-    class TestProgram extends PyTestProgram<I, O> {
-      protected override caseExecutionCode(data: I): string {
-        return generateCaseExecutionCode(data);
-      }
-    }
-    const pyRuntime = await pyRuntimeFactory(
-      ctx,
-      this.streams,
-      (ctx, imports) =>
-        WebAssembly.instantiateStreaming(
-          fetch(wasmUrl, { signal: ctx.signal }),
-          imports
-        ),
-      stdlibUrl
-    );
-    return {
-      async compile(_, files) {
-        if (files.length !== 1) {
-          throw new Error("Compilation of multiple files is not implemented");
-        }
-        return new TestProgram(pyRuntime, files[0].content);
-      },
-    };
-  }
+	constructor(protected readonly streams: Streams) {
+		this.logger = createLogger(streams.out);
+	}
+
+	async create<I, O>(
+		ctx: Context,
+		generateCaseExecutionCode: GenerateCaseExecutionCode<I>
+	): Promise<TestCompiler<I, O>> {
+		class TestProgram extends PyTestProgram<I, O> {
+			protected override caseExecutionCode(data: I): string {
+				return generateCaseExecutionCode(data);
+			}
+		}
+		const fetcher = createCachedFetch(await caches.open('python-cache'));
+		const pyRuntime = await pyRuntimeFactory(
+			ctx,
+			this.streams,
+			(ctx, imports) =>
+				WebAssembly.instantiateStreaming(fetcher(wasmUrl, { signal: ctx.signal }), imports),
+			stdlibUrl
+		);
+		this.logger.info(`Loaded ${wasmUrl}`);
+		return {
+			async compile(_, files) {
+				if (files.length !== 1) {
+					throw new Error('Compilation of multiple files is not implemented');
+				}
+				return new TestProgram(pyRuntime, files[0].content);
+			}
+		};
+	}
 }
