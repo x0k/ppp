68375f1ef6c750e880611f677d08e8f02affd813
Roman Krasilnikov <x0kill@ya.ru>
2026-02-09 16:17:00 +0300

    [python] Set `isatty=true`

--- packages/python-runtime/src/py-program.ts
+++ packages/python-runtime/src/py-program.ts
@@ -5,7 +5,7 @@ import { inContext, type Context } from "libs/context";
 export class PyProgram implements Program {
   constructor(
     protected readonly code: string,
-    protected readonly python: Awaited<ReturnType<typeof loadPyodide>>
+    protected readonly python: Awaited<ReturnType<typeof loadPyodide>>,
   ) {}
 
   async run(ctx: Context): Promise<void> {
